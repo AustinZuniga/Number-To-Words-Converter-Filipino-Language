db6191b5f15afef10b7be47227c3933eddb8bada
AustinZuniga <earlaustinavilazuniga@gmail.com>
2019-02-17 09:02:53 +0800

    fixed mispelled numbers

--- Converter.cc
+++ Converter.cc
@@ -1,44 +1,48 @@
 #include <vector>
 #include <iostream>
 
-
 std::string number_to_words( const unsigned int numero){
-    const std::vector<std::string> isa_sampu = {"sero", "isa", "dalawa", "tatlo", "apat", "lima", "anim", "pito", "walo", "siyam", "sampo"};
-    const std::vector<std::string> pang_dugtong = {"dalawang", "tatlong", "apatna", "limang", "animna", "pitong", "walong", "siyamna"};
-
+    const std::vector<std::string> isa_sampu = {"sero", "isa", "dalawa", "tatlo", "apat", "lima", "anim", "pito", "walo", "siyam", "sampu"};
+    const std::vector<std::string> pang_dugtong = {"dalawam", "tatlum", "apatna", "limam", "animna", "pitum", "walum", "siyamna"};
+    std::string to_concat;
     if( numero <= 10 ) // 0-10
         return isa_sampu.at(numero);
-    if( numero < 20 ) // 11 - 19
-        return "labing " + isa_sampu.at(numero-10);
+    if( numero < 20 ){ // 11 - 19
+        std::string at_less = isa_sampu.at(numero-10);
+        if(at_less == "isa" || at_less == "apat" || at_less == "anim")
+            to_concat = "labing-";
+        else
+            to_concat = "labing";
+        return to_concat + isa_sampu.at(numero-10);
+    }
     if( numero < 100 ) { // 20 - 99
         unsigned int remainder = numero - (static_cast<int>(numero/10)*10);
-        return pang_dugtong.at(numero/10-2) + (0 != remainder ? "pu't-" + number_to_words(remainder) : "pu");
+        return pang_dugtong.at(numero/10-2) + (0 != remainder ? "pu't " + number_to_words(remainder) : "pu");
     }
     if( numero < 1000 ) { // hundreds
         unsigned int remainder = numero - (static_cast<int>(numero/100)*100);
         std::string numm = isa_sampu.at(numero/100);
-        std::string to_concat;
         if(numm == "apat" || numm == "anim" || numm == "siyam")
-            to_concat = " na daan";
+            to_concat = " na raa";
         else
-            to_concat = "ng daan";
-        return isa_sampu.at(numero/100) + (0 != remainder ? to_concat+"g " + number_to_words(remainder) : to_concat);
+            to_concat = "ng daa";
+        return isa_sampu.at(numero/100) + (0 != remainder ? to_concat+"'t " + number_to_words(remainder) : to_concat+"n");
     }
     if( numero < 1000000 ) { // thousands
         unsigned int thousands = static_cast<int>(numero/1000);
         unsigned int remainder = numero - (thousands*1000);
-        std::string to_concat;
+    
         if(thousands == 4 || thousands == 6 || thousands == 9)
             to_concat = " na libo";
         else
-            to_concat = "ng libo";
+            to_concat = "g libo";
         return number_to_words(thousands) + (0 != remainder ? to_concat+"'t " + number_to_words(remainder) : to_concat);
     }
     if( numero < 1000000000 ) { // millions
         unsigned int millions = static_cast<int>(numero/1000000);
         unsigned int remainder = numero - (millions*1000000);
         std::string numm = number_to_words(millions);
-        std::string to_concat;
+        
         if(numm == "apat" || numm == "anim" || numm == "siyam")
             to_concat = " na milyong ";
         else
